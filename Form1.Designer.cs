7d9e2d960bc222a736e00d01efbcc5b27d7e8e98
liam-kilsdonk <94444182+liam-kilsdonk@users.noreply.github.com>
2023-03-30 15:34:44 +0200

    begin met structuur en test methode

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,25 +34,26 @@
             this.process1 = new System.Diagnostics.Process();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(212, 251);
+            this.button1.Location = new System.Drawing.Point(200, 385);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "pauze";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(459, 251);
+            this.button2.Location = new System.Drawing.Point(333, 385);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 29);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "play";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -69,10 +70,11 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(256, 349);
+            this.progressBar1.Location = new System.Drawing.Point(200, 429);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(243, 29);
+            this.progressBar1.Size = new System.Drawing.Size(353, 29);
             this.progressBar1.TabIndex = 2;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
             // imageList1
             // 
@@ -80,16 +82,28 @@
             this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(459, 385);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Replay";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(871, 598);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -101,5 +115,6 @@
         private System.Diagnostics.Process process1;
         private ProgressBar progressBar1;
         private ImageList imageList1;
+        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,14 +5,36 @@ namespace SpotifyLiam
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        public static void test()
+        {
+            Console.Beep();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            //pauze
+            test();
         }
 
         private void button2_Click(object sender, EventArgs e)
+        {
+            //play
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Replay
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            //de laad bar
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
